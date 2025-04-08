f3f47b09e90c2e7c36d5f0b06a0163fd6094ea37
jaydevd <jaydevd@zignuts.com>
2025-04-08 10:36:59 +0530

    Changes commited

--- api/routes/index.js
+++
@@ -1,8 +0,0 @@
-const ChatRoutes = require('./chats/ChatRoutes');
-const express = require('express');
-
-const router = express.Router();
-
-router.use('/chat', ChatRoutes);
-
-module.exports = router;
